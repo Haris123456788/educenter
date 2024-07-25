23711636a6ae8a39ddfb320a3d23d91b8ec55bd8
Haris123456788 <waseemharis1122@gmail.com>
2024-07-25 20:44:21 +0500

    Pages Deleted

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
   <!-- Basic Page Needs
 	================================================== -->
   <meta charset="utf-8">
-  <title>Educenter - Education HTML Template</title>
+  <title>Educenter - Education Website</title>
 
   <!-- Mobile Specific Metas
 	================================================== -->
