74b2dc21f1266d43f27692f0a71728c3c4da0f1d
Haris123456788 <waseemharis1122@gmail.com>
2024-07-26 15:14:58 +0500

    course detail update

--- course_detail.php
+++ course_detail.php
@@ -145,53 +145,16 @@ $data = mysqli_fetch_assoc($result);
       <div class="col-12 mb-4">
         <h3 class="mb-3">Requirements</h3>
         <div class="col-12 px-0">
-          <div class="row">
-            <div class="col-md-6">
-              <ul class="list-styled">
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-              </ul>
-            </div>
-            <div class="col-md-6">
-              <ul class="list-styled">
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-              </ul>
-            </div>
-          </div>
+        <?php echo $data['requirements']; ?>
         </div>
       </div>
       <div class="col-12 mb-4">
         <h3 class="mb-3">How to Apply</h3>
-        <ul class="list-styled">
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
-            dolor. Lorem, ipsum.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
-            dolor. Lorem, ipsum.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
-            dolor. Lorem, ipsum.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
-            dolor. Lorem, ipsum.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
-            dolor. Lorem, ipsum.</li>
-        </ul>
+        <?php echo $data['apply']; ?>
       </div>
       <div class="col-12 mb-5">
         <h3>Fees and Funding</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-          anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-          ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-          labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <p> <?php echo $data['funding']; ?> </p>
       </div>
       <!-- teacher -->
       <div class="col-12">
